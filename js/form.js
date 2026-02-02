459fafd7df0d4094ce9e1a36ba89e75090d759ad
sladkomedovyaroslav <sladkomedov.yaroslav10@gmail.com>
2026-02-03 00:15:31 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -444,12 +444,4 @@ document.addEventListener('DOMContentLoaded', function() {
             }, 300);
         }
     });
-    
-    // Функция для форматирования даты
-    function formatDate(date) {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
-    }
 });
